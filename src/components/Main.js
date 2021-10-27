4208e168679d1e983a659a04e36e156608fe6006
mac <mac@macs-Mac-mini.local>
2021-10-27 12:52:02 -0400

    url update 2

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ import Show from "../pages/Show"
 function Main(props) {
   const [ people, setPeople ] = useState(null);
 
-  const URL = "https://express-react-oct.herokuapp.com/";
+  const URL = "https://express-react-lab-nir.herokuapp.com/";
 
   const getPeople = async () => {
     const response = await fetch(URL);
